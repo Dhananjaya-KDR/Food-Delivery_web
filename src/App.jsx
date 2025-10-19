f525996f12c24a19287771a5cc575de7100157ba
Dhananjaya-KDR <ranaweera5454@gmail.com>
2025-10-19 18:49:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './componets/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import placeOder from './pages/placeOder/placeOder'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Routes>
+        <Route path ='/' element ={<Home/>} />
+         <Route path ='/cart' element ={<Cart/>} />
+          <Route path ='/order' element ={<placeOder/>} />
+
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/pages/placeOder/placeOder.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './placeOder.css'
+
+const placeOder = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default placeOder
